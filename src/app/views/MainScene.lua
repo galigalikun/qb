e6a8abd0b9b8c197d7a4e22650cce21508feada9
akaishi takeshi <t_akaishi@cgate.jp>
2016-05-07 20:27:14 +0900

    cocos studio code build

--- src/app/views/MainScene.lua
+++ src/app/views/MainScene.lua
@@ -1,16 +1,9 @@
 
 local MainScene = class("MainScene", cc.load("mvc").ViewBase)
 
-function MainScene:onCreate()
-    -- add background image
-    display.newSprite("HelloWorld.png")
-        :move(display.center)
-        :addTo(self)
+MainScene.RESOURCE_FILENAME = "MainScene.csb"
 
-    -- add HelloWorld label
-    cc.Label:createWithSystemFont("Hello World", "Arial", 40)
-        :move(display.cx, display.cy + 200)
-        :addTo(self)
+function MainScene:onCreate()
 
 end
 
