f322bbeedad2544bfcb82eb4476609f42717c8e4
akaishi takeshi <t_akaishi@cgate.jp>
2016-05-07 20:17:17 +0900

    sdkbox flurry

--- src/app/MyApp.lua
+++ src/app/MyApp.lua
@@ -3,6 +3,10 @@ local MyApp = class("MyApp", cc.load("mvc").AppBase)
 
 function MyApp:onCreate()
     math.randomseed(os.time())
+
+    sdkbox.PluginFlurryAnalytics:init()
+    sdkbox.PluginFlurryAnalytics:startSession()
+
 end
 
 return MyApp
