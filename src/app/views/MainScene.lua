99a56395de7650be4c4f3534a294d2c654b35259
akaishi takeshi <t_akaishi@cgate.jp>
2016-05-21 21:04:03 +0900

    text line height

--- src/app/views/MainScene.lua
+++ src/app/views/MainScene.lua
@@ -6,6 +6,8 @@ MainScene.RESOURCE_FILENAME = "scene/MainScene.csb"
 function MainScene:onCreate()
     local root = self:getResourceNode()
 
+    root:getChildByName("Text_1"):getVirtualRenderer():setLineHeight(20)
+
     root:getChildByName("Button_1"):addClickEventListener(function(sender)
         self:getApp():enterScene("GameScene")
     end)
