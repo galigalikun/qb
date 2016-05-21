25bb3700812735032d1b7dff7373d63fcd94f092
akaishi takeshi <t_akaishi@cgate.jp>
2016-05-21 21:12:52 +0900

    http request

--- src/app/views/MainScene.lua
+++ src/app/views/MainScene.lua
@@ -8,6 +8,18 @@ function MainScene:onCreate()
 
     root:getChildByName("Text_1"):getVirtualRenderer():setLineHeight(20)
 
+    local xhr = cc.XMLHttpRequest:new()
+    xhr.responseType = cc.XMLHTTPREQUEST_RESPONSE_BLOB
+    xhr:setRequestHeader("User-Agent", "todo")
+    xhr:open("GET", "https://www.google.co.jp")
+    xhr:registerScriptHandler(function()
+        dump(xhr)
+        if xhr.readyState == 4 and (xhr.status >= 200 and xhr.status < 207) then
+        end
+        xhr:unregisterScriptHandler()
+    end)
+    xhr:send()
+
     root:getChildByName("Button_1"):addClickEventListener(function(sender)
         self:getApp():enterScene("GameScene")
     end)
