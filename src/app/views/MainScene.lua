fb5f406f0a440f78c7a5c7948574cd5c7e6efb2f
akaishi takeshi <t_akaishi@cgate.jp>
2016-05-21 21:38:15 +0900

    promise test

--- src/app/views/MainScene.lua
+++ src/app/views/MainScene.lua
@@ -3,10 +3,29 @@ local MainScene = class("MainScene", cc.load("mvc").ViewBase)
 
 MainScene.RESOURCE_FILENAME = "scene/MainScene.csb"
 
+local deferred = require("packages.promises.deferred")
+
 function MainScene:onCreate()
     local root = self:getResourceNode()
 
     root:getChildByName("Text_1"):getVirtualRenderer():setLineHeight(20)
+
+    self:promisetest()
+        :next(function(obj)
+            dump(obj)
+        end,
+        function()
+            printInfo("reject")
+        end)
+
+
+    root:getChildByName("Button_1"):addClickEventListener(function(sender)
+        self:getApp():enterScene("GameScene")
+    end)
+end
+
+function MainScene:promisetest()
+    local d = deferred.new()
 
     local xhr = cc.XMLHttpRequest:new()
     xhr.responseType = cc.XMLHTTPREQUEST_RESPONSE_JSON
@@ -16,15 +35,15 @@ function MainScene:onCreate()
         dump(xhr)
         if xhr.readyState == 4 and (xhr.status >= 200 and xhr.status < 207) then
             local obj = json.decode(xhr.response)
-            dump(obj)
+            d:resolve(obj)
+        else
+            d:reject()
         end
         xhr:unregisterScriptHandler()
     end)
     xhr:send()
 
-    root:getChildByName("Button_1"):addClickEventListener(function(sender)
-        self:getApp():enterScene("GameScene")
-    end)
+    return d
 end
 
 return MainScene
